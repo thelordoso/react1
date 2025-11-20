21732f0a6bb8dcf7077463a59986264f4eb7d5a7
Armando Gonzalez Vazquez <ing.armandoglez@gmail.com>
2025-11-19 22:39:47 -0600

    Install vite

---
+++ front/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <img
+        src="https://img.freepik.com/foto-gratis/primer-plano-rosa-roja-rocio-parte-superior-negra_181624-28079.jpg?semt=ais_hybrid&w=740&q=80"
+        alt="Rosas"
+        style={{
+          maxWidth: "80px",
+          height: "auto",
+          display: "block",
+          borderRadius: 10,
+        }}
+      />
+    </div>
+  );
+}
