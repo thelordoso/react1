bdfc3be6a352b3e942e62ee0fe28f57308ec8fbf
Armando Gonzalez Vazquez <ing.armandoglez@gmail.com>
2025-11-20 08:54:41 -0600

    Multi paginas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,6 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const routes = [
+  {
+    path: "/cosa",
+    element: <HomePage />,
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +20,7 @@ function App() {
   return (
     <>
       <div>
-        <HomePage />
+        <RouterProvider router={router} />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
